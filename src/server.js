c8d6e6f386cb2d2aaebbdf54bb57cd38f0f9d048
Roosterseatyou <ltbkthree@icloud.com>
2026-01-13 20:17:47 -0600

    Migrations run on startup.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,35 @@
 import app from "./app.js";
+import knex from "./config/knex.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function init() {
+  try {
+    console.log("Running Migrations...");
+    await knex.migrate.latest();
+    console.log("Migrations completed.");
+
+    const server = app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+    const shutdown = async () => {
+        console.log("Shutting down server...");
+        server.close(async () => {});
+        await knex.destroy();
+        console.log("Server shut down complete.");
+        process.exit(0);
+    }
+
+    process.on('SIGINT', shutdown);
+    process.on('SIGTERM', shutdown);
+  } catch (error) {
+    console.error("Error during server initialization:", error);
+    process.exit(1);
+  }
+}
+
+init();
